27d3eade3fada869a67ed57ea677e58812d22254
maldad <canond_7@hotmail.com>
2015-12-15 11:56:24 -0600

    cambios: tabs->botones, templates de todos los destinos de botones OK

--- inicio/views.py
+++ inicio/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render_to_response
 ##requiere un html para renderizaar la vista
 def index(request):
     return render_to_response('inicio/index.html') #mandar el template a utilizar
+
+def oferta(request):
+    return render_to_response('inicio/oferta.html')
+
+def galeria(request):
+    return render_to_response('inicio/galeria.html')
+
+def contacto(request):
+    return render_to_response('inicio/contacto.html')
