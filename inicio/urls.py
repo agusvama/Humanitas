f21ec575ad84970101c448abe27029f1dd8a3294
maldad <canond_7@hotmail.com>
2015-12-15 12:44:49 -0600

    agregando 3 imagenes a la galeria

--- inicio/urls.py
+++ inicio/urls.py
@@ -4,7 +4,7 @@ from .views import index
 urlpatterns = patterns('',
     url(r'^$' , 'inicio.views.index'), #dar la vista que creamos
     url(r'^oferta/' , 'inicio.views.oferta'),
-    url(r'^galeria/' , 'inicio.views.oferta'),
-    url(r'^contacto/' , 'inicio.views.oferta'),
+    url(r'^galeria/' , 'inicio.views.galeria'),
+    url(r'^contacto/' , 'inicio.views.contacto'),
 
 )
